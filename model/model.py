3a60c482326f0d4241da762c2a93095df1b1fb83
xingwu-c <xingwu@connect.hku.hk>
2023-09-09 04:13:22 +0000

    first commit

---
+++ model/model.py
@@ -0,0 +1,111 @@
+
+import sys
+import torch
+import argparse
+
+from tqdm import tqdm
+from datasets import load_from_disk
+from torch.utils.data import DataLoader
+from transformers import GPT2LMHeadModel, GPT2Config, GPT2Tokenizer
+
+########################################################
+# 0. parse args
+########################################################
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--model-folder', 
+                    type=str,
+                    default="/embedding/v-xingwuchen/ts_data/TinyStories/tmp_models")
+parser.add_argument('--model-name', 
+                    type=str,
+                    default="TinyStories")
+parser.add_argument('--vocab-size', 
+                    type=int,
+                    default=50257,
+                    help='Vocabulary size of the GPT-2 model')
+parser.add_argument('--n-embd', 
+                    type=int,
+                    default=None,
+                    help='Hidden size of the transformer embeddings')
+parser.add_argument('--n-layer', 
+                    type=int,
+                    default=None,
+                    help='Number of transformer layers')
+parser.add_argument('--n-head', 
+                    type=int,
+                    default=None,
+                    help='Number of attention heads')
+parser.add_argument('--n-positions', 
+                    type=int,
+                    default=2048,
+                    help='Maximum sequence length')
+args = parser.parse_args()
+
+########################################################
+# 1. initialize models and dataset
+########################################################
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+
+
+config = GPT2Config(
+    vocab_size=args.vocab_size,
+    n_embd=args.n_embd,
+    n_layer=args.n_layer,
+    n_head=args.n_head,
+    n_positions=args.n_positions,
+)
+
+# creating the model
+model = GPT2LMHeadModel(config)
+
+print(model)
+
+device = "cuda"
+model.to(device)
+
+dataset = load_from_disk("/embedding/v-xingwuchen/ts_data/TinyStories/dataset/TinyStories")
+train_loader = DataLoader(dataset['train'], batch_size=12, shuffle=True)
+valid_loader = DataLoader(dataset['validation'], batch_size=24, shuffle=True)
+
+########################################################
+# 2. train from scarch
+########################################################
+
+optim = torch.optim.Adam(model.parameters(), lr=1e-3) # default lr ,betas and eps
+
+tokenizer.pad_token = tokenizer.eos_token
+best_loss = 1000
+updates = 0
+for epoch in range(10):
+    print(f"epoch : {epoch}")
+    print(f"{'*'*45}-train-{epoch:02}-{'*'*45}")
+    for batch in tqdm(train_loader):
+        optim.zero_grad()
+        tokenized = tokenizer(batch['text'], padding=True, return_tensors='pt',max_length = 512,truncation = True)['input_ids'].to(device)
+        loss = model(tokenized,labels = tokenized)["loss"]
+        loss.backward()
+        optim.step()
+        updates += 1
+        # print(f"train-{epoch:02}-{updates} : {loss.item()}")
+        if updates % 50 == 0 :
+            sys.stdout.flush()
+            tqdm.write(f"train-{epoch+1:02}-{updates} : {loss.item()}")
+            # print(f"train-{epoch:02}-{updates} : {loss.item()}")
+        del tokenized,loss
+        torch.cuda.empty_cache()
+    print("*"*100)
+    with torch.no_grad():
+        print(f"{'-'*45}-valid-{epoch+1:02}-{'-'*45}")
+        loss_valid = 0
+        for batch in tqdm(valid_loader):
+            tokenized = tokenizer(batch['text'], padding=True, return_tensors='pt',max_length = 1024,truncation = True)['input_ids'].to(device)
+            loss_valid += model(tokenized,labels = tokenized)["loss"].item()
+            del tokenized
+            torch.cuda.empty_cache()
+        print(loss_valid)
+        print("-"*100)
+        if best_loss > loss_valid:
+            model.save_pretrained(f"{args.model_folder}/{args.model_name}_best")
---
+++ model/train.sh
@@ -0,0 +1,30 @@
+# save model path
+rm -rf /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models
+mkdir -p /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models
+cd $(dirname "$0")
+
+# python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+#                     --n-embd 64 --n-layer 8 --n-head 16 --model-name TinyStories-1M
+
+# python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+#                     --n-embd 128 --n-layer 8 --n-head 16 --model-name TinyStories-3M
+
+python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+                    --n-embd 256 --n-layer 8 --n-head 16 --model-name TinyStories-8M | tee ../log/TinyStories-8M.log
+
+# python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+#                     --n-embd 512 --n-layer 8 --n-head 16 --model-name TinyStories-28M \
+                    # | tee ../log/TinyStories-28M.log
+
+# python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+#                     --n-embd 768 --n-layer 8 --n-head 16 --model-name TinyStories-33M
+
+# python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+#                     --n-embd 1024 --n-layer 1 --n-head 16 --model-name TinyStories-1Layer-21M
+
+# python model.py --vocab-size 50257 --n-positions 2048 --model-folder /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models\
+#                     --n-embd 1024 --n-layer 2 --n-head 16 --model-name TinyStories-2Layers-33M
+
+du -sh /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models/*
+
+rm -rf /embedding/v-xingwuchen/ts_data/TinyStories/tmp_models
